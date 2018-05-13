7a173d4c6c353b4e1502d938bb168bc00addc1f8
Alexei <A@Alexeis-MacBook-Pro-2.local>
2018-05-13 13:11:56 +0300

    Extra task added

--- app/src/UserList/UserListCtrl.js
+++ app/src/UserList/UserListCtrl.js
@@ -1,18 +1,27 @@
 'use strict'
 
-userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService) {
-  $scope.usersLoaded = false;
-  $scope.postsLoaded = false;
+userApp.controller('UserListCtrl', function ($scope, UsersService, PostsService, $q) {
+  $scope.loaded = false;
 
-  UsersService.getUsers().then(function (response) {
-    $scope.users = response.data;
-    $scope.usersLoaded = true;
-  })
+  $q.all([UsersService.getUsers(), PostsService.getPosts()]).then(function(response) {
 
-  PostsService.getPosts().then(function (response) {
-    $scope.posts = response.data;
-    $scope.postsLoaded = true;
-  })
+    $scope.users = response[0].data;
+    $scope.posts = response[1].data;
+
+    $scope.loaded = true;
+  });
+
+  // $scope.usersLoaded = false;
+  // $scope.postsLoaded = false;
+  // UsersService.getUsers().then(function (response) {
+  //   $scope.users = response.data;
+  //   $scope.usersLoaded = true;
+  // })
+  //
+  // PostsService.getPosts().then(function (response) {
+  //   $scope.posts = response.data;
+  //   $scope.postsLoaded = true;
+  // })
 
 
 /*   UsersService.getUsers().then(function (response) {
